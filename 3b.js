c783804648d7edf6894e9efb9b8a668382c76903
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-03 09:21:17 -0600

    Finding the golden claim

---
+++ 3b.js
@@ -0,0 +1,79 @@
+const fs = require('fs'),
+    _ = require('lodash');
+
+class Solution {
+    run() {
+        const input = fs.readFileSync('./3.dat', 'utf8');
+
+        const result = this.solve(input);
+        console.log('Result:', result);
+    }
+
+    solve(input) {
+        const claims = this.parseInput(input);
+        const size = this.getFabricBounds(claims);
+
+        let grid = new Array(size);
+        for (let i = 0; i < grid.length; i++) {
+            grid[i] = _.fill(Array(size), 0);
+        }
+
+        _.forEach(claims, claim => {
+            for (let x = claim.left; x < claim.left + claim.width; x++) {
+                for (let y = claim.top; y < claim.top + claim.height; y++) {
+                    grid[y][x] += 1;    // increment every square inch for every claim
+                }
+            }
+        });
+
+        _.forEach(claims, claim => {
+            let theGoldenClaim = true;
+            for (let x = claim.left; x < claim.left + claim.width; x++) {
+                for (let y = claim.top; y < claim.top + claim.height; y++) {
+                    if(grid[y][x] !== 1) theGoldenClaim = false;
+                }
+            }
+
+            if(theGoldenClaim) {
+                return claim.claim;
+            };
+        });
+    }
+
+    parseInput(input) {
+        const exp = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/;
+
+        const arr = _(input)
+            .chain()
+            .split('\n')    // split by line break
+            .map(x => {
+                const params = x.match(exp);
+
+                return {
+                    raw: params[0],
+                    claim: +params[1],
+                    left: +params[2],
+                    top: +params[3],
+                    width: +params[4],
+                    height: +params[5]
+                };
+            })
+            .value();
+
+        return arr;
+    }
+
+    getFabricBounds(claims) {
+        const maxWidthClaim = _.maxBy(claims, x => x.left + x.width);
+        const maxHeightClaim = _.maxBy(claims, x => x.top + x.height);
+
+        const maxWidth = maxWidthClaim.left + maxWidthClaim.width;
+        const maxHeight = maxHeightClaim.top + maxHeightClaim.height
+
+        const size = Math.max(maxWidth, maxHeight);
+
+        return size;
+    }
+}
+
+module.exports = Solution;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const solution = new (require('./3a'))();
+const solution = new (require('./3b'))();
 
 // custom lodash functions
 (function() {
