53721787252255bd5d781effc4ed925d944a789b
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-06 07:58:50 -0600

    WIP

--- 6a.js
+++ 6a.js
@@ -16,21 +16,70 @@ class Solution {
     }
 
     solve(input) {
+        let i = 0;
         const coords = _(input)
             .chain()
             .split('\n')
             .map(_ => _.split(', '))
             .map(_ => ({
+                id: i++,
                 x: +_[0],
                 y: +_[1]
             }))
             .value();
 
+        const boundingBox = this.getBoundingBox(coords);
+
+        let rows = new Array(boundingBox.y);
+        for (let i = 0; i < boundingBox.y; i++) {
+            rows[i] = _.fill(Array(boundingBox.x), null);
+        }
+
+        for (let x = 0; x < boundingBox.x; x++) {
+            for (let y = 0; y < boundingBox.y; y++) {
+                const distances = _(coords)
+                    .chain()
+                    .map(_ => ({
+                        point: _,
+                        distance: this.getManhattanDistance(_, { x, y })
+                    }))
+                    .orderBy(_ => _.distance)
+                    .value();
+
+                const minDistance = _.minBy(distances, _ => _.distance).distance;
+
+                const points = _.filter(distances, _ => _.distance === minDistance);
+
+                if (points.length === 1)
+                    rows[y][x] = points[0].point.id;
+                else rows[y][x] = null;
+            }
+        }
+
         return null;
     }
 
+    getBoundingBox(coords) {
+        const x = _(coords)
+            .chain()
+            .map(_ => _.x)
+            .max()
+            .value() + 1;
+
+        const y = _(coords)
+            .chain()
+            .map(_ => _.y)
+            .max()
+            .value() + 1;
+
+        return { x, y };
+    }
+
     getManhattanDistance(pointA, pointB) {
-        
+        const x = Math.abs(pointA.x - pointB.x);
+        const y = Math.abs(pointA.y - pointB.y);
+
+        return x + y;
     }
 }
 
