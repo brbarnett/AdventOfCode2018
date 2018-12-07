1769194e3fa61f1cdb159c40fe0575afad45f0a6
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-07 14:19:33 -0600

    Finished 7a

--- 7a.js
+++ 7a.js
@@ -4,13 +4,6 @@ const fs = require('fs'),
 class Solution {
     run() {
         const input = fs.readFileSync('./7.dat', 'utf8');
-        // const input = `Step C must be finished before step A can begin.
-        // Step C must be finished before step F can begin.
-        // Step A must be finished before step B can begin.
-        // Step A must be finished before step D can begin.
-        // Step B must be finished before step E can begin.
-        // Step D must be finished before step E can begin.
-        // Step F must be finished before step E can begin.`;
 
         this.dependencies = _(input)
             .chain()
@@ -22,15 +15,27 @@ class Solution {
 
         const result = this.solve();
         console.log('Result:', result);
-
-        // not BGKDZWMJCNEUYQSTRHLPAFIVXO
-        // not BQRGKDZWHMJCNEUSTYLPAFIVXO
     }
 
     solve() {
-        const start = _.difference(_.uniq(this.dependencies.map(_ => _.dependency)), _.uniq(this.dependencies.map(_ => _.step))).sort();
+        const available = _.difference(_.uniq(this.dependencies.map(_ => _.dependency)), _.uniq(this.dependencies.map(_ => _.step)));
 
-        start.forEach(_ => this.findNextSteps(_));
+        do {
+            const step = available.sort()[0];
+            const hasAllDependencies = _(this.dependencies)
+                .chain()
+                .filter(_ => _.step === step)
+                .every(_ => this.steps.has(_.dependency))
+                .value();
+
+            if (hasAllDependencies)
+                this.steps.add(step);
+
+            _.remove(available, _ => _ === step);
+
+            available.push(...this.dependencies.filter(_ => _.dependency === step).map(_ => _.step));
+        }
+        while (available.length > 0)
 
         return [...this.steps].join('');
     }
@@ -41,21 +46,6 @@ class Solution {
             step: parsed[2],
             dependency: parsed[1]
         };
-    }
-
-    findNextSteps(step) {
-        const hasAllDependencies = _(this.dependencies)
-            .chain()
-            .filter(_ => _.step === step)
-            .every(_ => this.steps.has(_.dependency))
-            .value();
-
-        if (hasAllDependencies)
-            this.steps.add(step);
-
-        const nextSteps = this.dependencies.filter(_ => _.dependency === step).map(_ => _.step).sort();
-
-        nextSteps.forEach(_ => this.findNextSteps(_));
     }
 }
 
