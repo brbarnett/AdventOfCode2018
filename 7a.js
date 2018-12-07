5f55f4411434db9a748f7a4503aa6b9183b68e29
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-07 07:29:18 -0600

    WIP 7a

--- 7a.js
+++ 7a.js
@@ -4,18 +4,56 @@ const fs = require('fs'),
 class Solution {
     run() {
         const input = fs.readFileSync('./7.dat', 'utf8');
+        // const input = `Step C must be finished before step A can begin.
+        // Step C must be finished before step F can begin.
+        // Step A must be finished before step B can begin.
+        // Step A must be finished before step D can begin.
+        // Step B must be finished before step E can begin.
+        // Step D must be finished before step E can begin.
+        // Step F must be finished before step E can begin.`;
 
-        const result = this.solve(input);
+        this.dependencies = _(input)
+            .chain()
+            .split('\n')
+            .map(_ => this.parse(_))
+            .value();
+
+        this.steps = [];
+
+        const result = this.solve();
         console.log('Result:', result);
     }
 
-    solve(input) {
-        const coords = _(input)
-            .chain()
-            .split('\n')
+    solve() {
+        const start = _.difference(_.uniq(this.dependencies.map(_ => _.dependency)), _.uniq(this.dependencies.map(_ => _.step))).sort();
 
-            .value();
+        this.findNextSteps(start[0], 0);
 
+        const steps = this.steps.map(_ => _.step);
+        const last = steps.reverse()[0];
+        _.remove(steps, _ => _ === last);
+        steps.reverse().push(last);
+
+        return _.uniq(steps).join('');
+    }
+
+    parse(instruction) {
+        const parsed = instruction.match(/Step (\w) must be finished before step (\w) can begin/);
+        return {
+            step: parsed[2],
+            dependency: parsed[1]
+        };
+    }
+
+    findNextSteps(step, level) {
+        this.steps.push({
+            step: step,
+            level: level
+        });
+
+        const nextSteps = this.dependencies.filter(_ => _.dependency === step).map(_ => _.step).sort();
+
+        nextSteps.forEach(_ => this.findNextSteps(_, level + 1));
     }
 }
 
