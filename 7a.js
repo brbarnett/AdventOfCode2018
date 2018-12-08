e79e3bcef2e83d25ba7f39a63e9d21d2b02f9898
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-07 19:38:42 -0600

    Cleaning up

--- 7a.js
+++ 7a.js
@@ -5,39 +5,38 @@ class Solution {
     run() {
         const input = fs.readFileSync('./7.dat', 'utf8');
 
-        this.dependencies = _(input)
+        const result = this.solve(input);
+        console.log('Result:', result);
+    }
+
+    solve(input) {
+        let steps = new Set();
+        let dependencies = _(input)
             .chain()
             .split('\n')
             .map(_ => this.parse(_))
             .value();
 
-        this.steps = new Set();
-
-        const result = this.solve();
-        console.log('Result:', result);
-    }
-
-    solve() {
-        const available = _.difference(_.uniq(this.dependencies.map(_ => _.dependency)), _.uniq(this.dependencies.map(_ => _.step)));
+        let available = _.difference(_.uniq(dependencies.map(_ => _.dependency)), _.uniq(dependencies.map(_ => _.step)));
 
         do {
             const step = available.sort()[0];
-            const hasAllDependencies = _(this.dependencies)
+            const hasAllDependencies = _(dependencies)
                 .chain()
                 .filter(_ => _.step === step)
-                .every(_ => this.steps.has(_.dependency))
+                .every(_ => steps.has(_.dependency))
                 .value();
 
             if (hasAllDependencies)
-                this.steps.add(step);
+                steps.add(step);
 
             _.remove(available, _ => _ === step);
 
-            available.push(...this.dependencies.filter(_ => _.dependency === step).map(_ => _.step));
+            available.push(...dependencies.filter(_ => _.dependency === step).map(_ => _.step));
         }
         while (available.length > 0)
 
-        return [...this.steps].join('');
+        return [...steps].join('');
     }
 
     parse(instruction) {
