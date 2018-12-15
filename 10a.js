7b368cbfd19feddc7e0bf43ceb94cc4493e2526c
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-12-15 12:14:37 -0600

    10a

--- 10a.js
+++ 10a.js
@@ -3,38 +3,7 @@ const fs = require('fs'),
 
 class Solution {
     run() {
-        // const input = fs.readFileSync('./10.dat', 'utf8');
-        const input = `position=< 9,  1> velocity=< 0,  2>
-        position=< 7,  0> velocity=<-1,  0>
-        position=< 3, -2> velocity=<-1,  1>
-        position=< 6, 10> velocity=<-2, -1>
-        position=< 2, -4> velocity=< 2,  2>
-        position=<-6, 10> velocity=< 2, -2>
-        position=< 1,  8> velocity=< 1, -1>
-        position=< 1,  7> velocity=< 1,  0>
-        position=<-3, 11> velocity=< 1, -2>
-        position=< 7,  6> velocity=<-1, -1>
-        position=<-2,  3> velocity=< 1,  0>
-        position=<-4,  3> velocity=< 2,  0>
-        position=<10, -3> velocity=<-1,  1>
-        position=< 5, 11> velocity=< 1, -2>
-        position=< 4,  7> velocity=< 0, -1>
-        position=< 8, -2> velocity=< 0,  1>
-        position=<15,  0> velocity=<-2,  0>
-        position=< 1,  6> velocity=< 1,  0>
-        position=< 8,  9> velocity=< 0, -1>
-        position=< 3,  3> velocity=<-1,  1>
-        position=< 0,  5> velocity=< 0, -1>
-        position=<-2,  2> velocity=< 2,  0>
-        position=< 5, -2> velocity=< 1,  2>
-        position=< 1,  4> velocity=< 2,  1>
-        position=<-2,  7> velocity=< 2, -2>
-        position=< 3,  6> velocity=<-1, -1>
-        position=< 5,  0> velocity=< 1,  0>
-        position=<-6,  0> velocity=< 2,  0>
-        position=< 5,  9> velocity=< 1, -2>
-        position=<14,  7> velocity=<-2,  0>
-        position=<-3,  6> velocity=< 2, -1>`;
+        const input = fs.readFileSync('./10.dat', 'utf8');
 
         this.writeResult(this.solve(input));
     }
@@ -51,7 +20,7 @@ class Solution {
 
         let history = [arr];
         while (true) {
-            let local = history[history.length - 1].slice(0);   // clone last array
+            let local = JSON.parse(JSON.stringify(history[history.length - 1]));   // deep clone last array
             _.forEach(local, _ => {
                 _.position.x += _.velocity.x;
                 _.position.y += _.velocity.y;
